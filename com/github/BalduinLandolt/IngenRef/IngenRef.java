70b7f427570d9cabd918ff7abc6c6e483ba200ae
blandolt <balduin.landolt@unibas.ch>
2019-04-09 23:01:30 +0200

    Add: Created runner class.

---
+++ com/github/BalduinLandolt/IngenRef/IngenRef.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.github.BalduinLandolt.IngenRef;
+
+/**
+ * @author Balduin Landolt
+ * 
+ * Main Class for IngenRef. <br/>
+ * 
+ * Does nothing, but run the actual application.
+ */
+public class IngenRef {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Run program here
+
+	}
+
+}
