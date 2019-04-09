0296044487756a7a0c6db44c34ed0d8ec8fbe997
blandolt <balduin.landolt@unibas.ch>
2019-04-09 23:20:58 +0200

    Fix: undid some messy structure.

---
+++ src/main/java/com/github/BalduinLandolt/IngenRef/IngenRef.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.github.BalduinLandolt.IngenRef;
+
+/**
+ * @author Balduin Landolt
+ * 
+ * Runner for IngenRef. <br/>
+ * 
+ * Does nothing, except running the actual application.
+ *
+ *
+ */
+public class IngenRef {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test");
+
+	}
+
+}
